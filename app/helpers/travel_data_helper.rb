4576a031b77a938816fcc7d3c85f9b7d8e692ff1
Artiom Fine <art@finecomputing.com>
2012-01-07 23:06:50 -0500

    Cleaned up code in travel_datum view

--- app/helpers/travel_data_helper.rb
+++ app/helpers/travel_data_helper.rb
@@ -1,2 +1,14 @@
 module TravelDataHelper
+	def get_directions_url(destinationAddress)
+		googleLink ="http://maps.google.com/maps?"
+		currUrl = googleLink+"saddr="+stingToURLParameter(Constant.first.home_address)
+		currUrl= currUrl+"&daddr="+stingToURLParameter(destinationAddress)
+	end
+	
+	def stingToURLParameter(param)
+		toReturn=param.clone
+		toReturn.gsub!(", ","+")
+		toReturn.gsub!(" ","+")
+		return toReturn
+	end
 end
