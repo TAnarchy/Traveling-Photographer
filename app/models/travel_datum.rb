a506bfffc9d5c500d9917e1c582194ada8f1c961
Artiom Fine <art@finecomputing.com>
2012-01-13 20:29:05 -0500

    Code clean up

--- app/models/travel_datum.rb
+++ app/models/travel_datum.rb
@@ -1,35 +1,32 @@
-require 'MyWeb'
+require 'GoogleMapsWebServicesWrapper'
 class TravelDatum < ActiveRecord::Base
-	before_save :caclFormulaFields
-	validates :Address, :presence => true
+  before_save :calculate_calculated_fields
+  validates :Address, :presence => true
 	
-	def caclFormulaFields
-		@constant = Constant.first
-		 mt= MyWeb.new(@constant.home_address,self.Address)
-		 splt= mt.getDistance.split(" ")
-		 splt2= mt.getDuration
-		 self.DistanceFromHome=splt[0]
-		 self.TravelTime=splt2
-		 self.Departure=self.StartTime-self.TravelTime.to_i.minutes-1.hour-40.minutes
-		 mt2= MyWeb.new(self.Address, @constant.officeAdr)
-		 self.DistanceFromSchoolToOffice= mt2.getDistance.split(" ")[0]
-		 self.TimeFromSchoolToOffice= mt2.getDuration
-		 if self.isTravelingToOffice== "Yes"
-		 	 mt3 = MyWeb.new(@constant.officeAdr,@constant.home_address)
-		 	 dist = mt3.getDistance.split(" ")
-		 	 dist2 = dist[0]
-		 	stuff = self.gasprice*(self.DistanceFromSchoolToOffice.to_i+self.DistanceFromHome.to_i+dist2.to_i)
-		end
-		if self.isTravelingToOffice !="Yes"
-	 	 	 stuff = self.gasprice*(self.DistanceFromHome.to_i*2)
-	 	 end
-	 	 
-		 self.gastotal= (stuff/@constant.mpg).round(2)
-		 busHours =(self.EndOfBusiness.hour-self.Departure.hour).to_f
-		 busMin = ((self.EndOfBusiness.min-self.Departure.min).abs).to_f
-		 #self.HourlyRate= busMin*100/60
-		 busMin2 = (busMin/60).to_f
-		 self.HourlyRate=(125/(busHours+busMin2)).to_f.round(2)
-	end
-	#test
+  def calculate_calculated_fields
+    @constant = Constant.first
+    home_to_school_wrapper= GoogleMapsWebServicesWrapper.new(@constant.home_address,self.Address)
+    splt= home_to_school_wrapper.distance.split(" ")
+    splt2= home_to_school_wrapper.duration
+    self.DistanceFromHome=splt[0]
+    self.TravelTime=splt2
+    self.Departure=self.StartTime-self.TravelTime.to_i.minutes-1.hour-40.minutes
+    school_to_office_wrapper= GoogleMapsWebServicesWrapper.new(self.Address, @constant.officeAdr)
+    self.DistanceFromSchoolToOffice= school_to_office_wrapper.distance.split(" ")[0]
+    self.TimeFromSchoolToOffice= school_to_office_wrapper.duration
+    if self.isTravelingToOffice== "Yes"
+      office_to_home_wrapper = GoogleMapsWebServicesWrapper.new(@constant.officeAdr,@constant.home_address)
+      dist = office_to_home_wrapper.distance.split(" ")
+      dist2 = dist[0]
+      stuff = self.gasprice*(self.DistanceFromSchoolToOffice.to_i+self.DistanceFromHome.to_i+dist2.to_i)
+    end
+    if self.isTravelingToOffice !="Yes"
+      stuff = self.gasprice*(self.DistanceFromHome.to_i*2)
+   end
+   self.gastotal= (stuff/@constant.mpg).round(2)
+   busHours =(self.EndOfBusiness.hour-self.Departure.hour).to_f
+   busMin = ((self.EndOfBusiness.min-self.Departure.min).abs).to_f
+   busMin2 = (busMin/60).to_f
+   self.HourlyRate=(125/(busHours+busMin2)).to_f.round(2)
+  end
 end
---
+++ lib/GoogleMapsWebServicesWrapper.rb
@@ -0,0 +1,65 @@
+require 'net/http'
+require 'rexml/document'
+
+
+class GoogleMapsWebServicesWrapper
+	attr_accessor :duration, :distance
+  
+  def initialize(org,dest)
+    send_request(org,dest)
+    parse_xml
+    format_duration_and_distance
+  end
+		
+  def send_request(org,dest)
+    google_web_service_url = 'http://maps.googleapis.com/maps/api/distancematrix/xml'
+    sensor = 'false'
+    directions_origin = string_to_url_parameter(org)
+    directions_destination = string_to_url_parameter(dest)
+    units = 'imperial'
+    results_limit = 10
+    url = "#{google_web_service_url}?origins=#{directions_origin}&destinations=#{directions_destination}&units=#{units}&sensor=#{sensor}"
+    begin
+      @xml_result_set = Net::HTTP.get_response(URI.parse(url))
+    rescue Exception => e
+      puts 'Connection error: ' + e.message
+    end
+  end
+		
+  def parse_xml
+    doc = REXML::Document.new(@xml_result_set.body)
+    puts @xml_result_set.body
+    result = []
+    result2 = []
+    REXML::XPath.each( doc, "//duration/text") { |element| result << element.text }
+    @duration = result[0]
+    REXML::XPath.each( doc, "//distance/text") { |element2| result2 << element2.text }
+    @distance = result2[0]
+  end
+	
+  def format_duration_and_distance
+    if @duration.include?("hour")
+      time =@duration.split(" ")
+      hours = time[0].to_i
+      minutes = time[2].to_i
+      hours = hours * 60
+      @duration = hours+minutes
+    end
+		
+    if @distance.include?(",")
+      @distance[","]=""
+    end
+  end
+  def string_to_url_parameter(param)
+    to_return=param.clone
+    to_return.gsub!(", ","+")
+    to_return.gsub!(" ","+")
+    to_return
+  end
+	
+end
+
+
+
+
+
