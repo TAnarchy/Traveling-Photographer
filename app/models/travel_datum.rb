622857a008e46143f5bf70c7cabee3ed8e878b01
Artiom Fine <art@finecomputing.com>
2012-01-26 19:35:37 -0500

    Cleaned up travel_datum.rb

--- app/models/travel_datum.rb
+++ app/models/travel_datum.rb
@@ -2,28 +2,52 @@ require 'GoogleMapsWebServicesWrapper'
 class TravelDatum < ActiveRecord::Base
   before_save :process_calculated_fields
   validates :address, :presence => true
-	
+
   def process_calculated_fields
+    send_home_request
+    set_home_values
+    send_school_request
+    set_school_values
+    traveling_to_office_check
+    gas_calculations
+  end
+
+  def send_home_request
     @independent = TripIndependentInfo.first
-    home_to_school_wrapper= GoogleMapsWebServicesWrapper.new(@independent.home_address,self.address)
-    splt= home_to_school_wrapper.distance.split(" ")
-    splt2= home_to_school_wrapper.duration
+    @home_to_school_wrapper= GoogleMapsWebServicesWrapper.new(@independent.home_address,self.address)
+  end
+
+  def set_home_values
+    splt= @home_to_school_wrapper.distance.split(" ")
+    splt2= @home_to_school_wrapper.duration
     self.distance_from_home=splt[0]
     self.home_to_school_travel_time=splt2
     self.departure=self.start_time-self.home_to_school_travel_time.to_i.minutes-1.hour-40.minutes
-    school_to_office_wrapper= GoogleMapsWebServicesWrapper.new(self.address, @independent.office_address)
-    self.distance_from_school_to_office= school_to_office_wrapper.distance.split(" ")[0]
-    self.time_from_school_to_office= school_to_office_wrapper.duration
+  end
+
+  def send_school_request
+    @school_to_office_wrapper= GoogleMapsWebServicesWrapper.new(self.address, @independent.office_address)
+  end
+
+  def set_school_values
+    self.distance_from_school_to_office= @school_to_office_wrapper.distance.split(" ")[0]
+    self.time_from_school_to_office= @school_to_office_wrapper.duration
+  end
+
+  def traveling_to_office_check
     if self.is_traveling_to_office== "Yes"
       office_to_home_wrapper = GoogleMapsWebServicesWrapper.new(@independent.office_address,@independent.home_address)
       dist = office_to_home_wrapper.distance.split(" ")
       dist2 = dist[0]
-      stuff = self.gas_price*(self.distance_from_school_to_office.to_i+self.distance_from_home.to_i+dist2.to_i)
+      @stuff = self.gas_price*(self.distance_from_school_to_office.to_i+self.distance_from_home.to_i+dist2.to_i)
     end
     if self.is_traveling_to_office !="Yes"
-      stuff = self.gas_price*(self.distance_from_home.to_i*2)
+      @stuff = self.gas_price*(self.distance_from_home.to_i*2)
    end
-   self.total_trip_gas_cost= (stuff/@independent.mpg).round(2)
+  end
+
+def gas_calculations
+   self.total_trip_gas_cost= (@stuff/@independent.mpg).round(2)
    busHours =(self.end_of_business_time.hour-self.departure.hour).to_f
    busMin = ((self.end_of_business_time.min-self.departure.min).abs).to_f
    busMin2 = (busMin/60).to_f
