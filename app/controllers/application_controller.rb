4214fec29e202f82fc7529886c35d88db164ae52
Artiom Fine <art@finecomputing.com>
2012-02-05 14:53:17 -0500

    Deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #http_basic_authenticate_with :name =>User.first.user_name, :password =>User.first.password
+  http_basic_authenticate_with :name =>"frodo", :password =>"thering"
 end
