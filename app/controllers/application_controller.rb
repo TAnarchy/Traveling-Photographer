ce441f89a5fe78a6cd169a19ccca34f15b3cfea8
Artiom Fine <art@finecomputing.com>
2012-09-11 14:53:14 -0400

    Security changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
-  protect_from_forgery
-  #http_basic_authenticate_with :name =>"frodo", :password =>"thering"
+  #protect_from_forgery
 end
